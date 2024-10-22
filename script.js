1cff1e8cd97c86284f505084913fa478138e2df6
Tejas Lanjulkar <155477246+TejasLanjulkar@users.noreply.github.com>
2024-10-22 12:58:35 +0530

    Add files via upload
    Dom_Project 1 in js

---
+++ script.js
@@ -0,0 +1,13 @@
+let div = document.getElementById('myDiv');//Fetch the div element by its id
+
+div.style.backgroundColor = 'yellow';
+
+div.style.margin = '20px';
+
+ div.style.padding = '10px';
+
+ div.style.fontSize = '18px';
+ div.style.fontWeight = 'bold';
+ div.style.height = '200px';
+
+ div.style.width = '300px';
